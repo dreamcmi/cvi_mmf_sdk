02ee8d80a34daaa6a88d05f43d4d60c05db05426
dreamcmi <1912544842@qq.com>
2023-04-21 15:56:33 +0800

    support genimage to build the sd burn image

--- build/tools/common/sd_tools/sd_gen_burn_image.sh
+++ build/tools/common/sd_tools/sd_gen_burn_image.sh
@@ -14,69 +14,15 @@ then
 	exit
 fi
 
-vfat_cap=128M
-vfat_label="boot"
-ext4_cap=256M
-ext4_label="rootfs"
-
 output_dir=$1
 echo ${output_dir}
 pushd ${output_dir}
 
-# gen a empty image
-image=sophpi-duo-`date +%Y%m%d-%H%M`.img
-echo ${image}
-dd if=/dev/zero of=./${image} bs=1M count=512
+image=${PROJECT_FULLNAME}-`date +%Y%m%d-%H%M`-sdcard.zip
 
-################################
-# Note: do not change this flow
-################################
-sudo fdisk ./${image} << EOF
-n
-p
-1
+genimage --config genimage.cfg --rootpath fs/ --tmppath `mktemp -d`  --inputpath $PWD --outputpath $PWD
 
-+${vfat_cap}
-n
-p
-2
-
-+${ext4_cap}
-w
-EOF
-# Note end
-################################
-
-dev_name=`sudo losetup -f`
-echo ${dev_name}
-echo ""
-
-sudo losetup ${dev_name} ./${image}
-sudo partprobe ${dev_name}
-
-sudo mkfs.vfat -F 32 -n ${vfat_label} ${dev_name}p1
-sudo mkfs.ext4 -L ${ext4_label} ${dev_name}p2
-
-# mount partitions
-rm ./tmp1 ./tmp2 -rf
-mkdir tmp1 tmp2
-sudo mount -t vfat ${dev_name}p1 tmp1/
-sudo mount -t ext4 ${dev_name}p2 tmp2/
-
-# copy boot file and rootfs
-sudo cp ${output_dir}/fip.bin ./tmp1/
-sudo cp ${output_dir}/rawimages/boot.sd ./tmp1/
-sudo cp -raf ${output_dir}/fs/* ./tmp2
-
-sync
-
-# umount
-sudo umount tmp1 tmp2
-sudo losetup -d ${dev_name}
-rmdir tmp1 tmp2
-
-# tar image
-tar zcvf ${image}.tar.gz ${image}
+zip ${image} sdcard.img
 
 echo "Gen image successful: ${image}"
 echo ""
